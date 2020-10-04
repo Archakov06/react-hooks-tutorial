aca2af60b777a2c96a9fcd7851cc9610cce31a9f
Archakov Dennis <amonbower@gmail.com>
2020-10-04 20:42:32 +0300

    init useRef

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
+/* eslint-disable jsx-a11y/accessible-emoji */
 import React from 'react';
-import List from './List';
 
 function App() {
-  const [visibleList, setVisibleList] = React.useState(true);
+  const [numbers, setNumbers] = React.useState([1, 2, 3, 4, 5]);
+  const timerRef = React.useRef();
 
-  const toggleVisibleList = () => {
-    setVisibleList((visible) => !visible);
+  const addNumber = () => {
+    setNumbers((prev) => [...prev, prev[prev.length - 1] + 1]);
+  };
+
+  const start = () => {
+    timerRef.current = setInterval(addNumber, 1000);
+  };
+
+  const stop = () => {
+    console.log(timerRef.current);
+    clearInterval(timerRef.current);
   };
 
   return (
-    <div className="App">
-      {visibleList && <List />}
-      <button onClick={toggleVisibleList}>Показать / Скрыть список</button>
+    <div>
+      <ul>
+        {numbers.map((n) => (
+          <li key={n}>{n}</li>
+        ))}
+      </ul>
+      <button onClick={addNumber}>✅ Добавить число</button>
+      <button onClick={start}>▶️ Старт</button>
+      <button onClick={stop}>⏹ Стоп</button>
     </div>
   );
 }
